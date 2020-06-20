74d600083b821888f222a7f76fe17fa160447381
grantkoji <grantkoji@gmail.com>
2020-06-19 21:30:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards(namesArray, event) {
+    let message = []
+    for (let i = 0; i < namesArray.length; i++ ) {
+        message.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return message
+}
+
+function countDown(number) {
+    while (number >= 0 ) {
+        console.log( number );
+        number -= 1;
+    }
+}
+
