8a5813a2b30e0ba387c1620c5cbf81e8d1d53638
cathjime <xcatherine.jimenez@gmail.com>
2020-06-20 13:35:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        name = names[i];
+        newArray.push(`Thank you, ${name}, for the wonderful ${event} gift!`); 
+    }
+    return newArray;
+}
+
+function countDown(num){
+    let i = 0;
+    while (i <= num) {
+        console.log(i);
+        i++;
+    }
+}
