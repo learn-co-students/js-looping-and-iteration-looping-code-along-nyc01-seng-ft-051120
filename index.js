6b8dba6a01f86cde7741e1bcdd4d4c3b9cc6a2ce
JulianCedric <julian.pormentilla@gmail.com>
2020-06-21 19:47:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+// 
+
+function writeCards( namesArray, event ) {
+    // functions perform the same function as Ruby's 'methods'
+
+    let thankYouCards = []
+        // create new empty array, to be returned at the end of this function
+
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
