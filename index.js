15fc77d3854ced2aaa8272ac8de0669a1cebe11f
Gary Cordero <GaryCordero1690@gmail.com>
2020-06-22 14:29:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array,message) {
+  let response = [];
+  for (let i = 0;  i <array.length ; i++ ){
+    response.push(`Thank you, ${array[i]}, for the wonderful ${message} gift!`);
+  }
+  return response
+}
+
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+function countDown(num){
+  while (num >= 0){
+    console.log(num--);
+  }
+}
+
+countDown(10);
